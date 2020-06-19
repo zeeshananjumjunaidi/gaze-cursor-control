7fb2ccdd33d808eed22cb48a0158c846ee3bf969
zeeshananjumjunaidi <zeeshananjumjunaidi@gmail.com>
2020-06-19 18:43:36 +0300

    fix run.sh input variable

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ python_version=3.5
 # Sourcing openvino
 source /opt/intel/openvino/bin/setupvars.sh
 
-FACE_DETECTION_MODEL="intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml"
+FACE_DETECTION_MODEL="model/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001.xml"
 # Using 16FP model for gaze estimation
-GAZE_ESTIMATION_MODEL="intel/gaze-estimation-adas-0002/FP16/gaze-estimation-adas-0002.xml"
+GAZE_ESTIMATION_MODEL="model/intel/gaze-estimation-adas-0002/FP16/gaze-estimation-adas-0002.xml"
 # Using 16FP model for head pose estimation
-HEAD_POSE_ESTIMATION_MODEL="intel/head-pose-estimation-adas-0001/FP16/head-pose-estimation-adas-0001.xml"
+HEAD_POSE_ESTIMATION_MODEL="model/intel/head-pose-estimation-adas-0001/FP16/head-pose-estimation-adas-0001.xml"
 # Using 16FP model for landmkars regression model
-LANDMARKS_REGRESSION_MODEL="intel/landmarks-regression-retail-0009/FP16-INT8/landmarks-regression-retail-0009.xml"
+LANDMARKS_REGRESSION_MODEL="model/intel/landmarks-regression-retail-0009/FP16-INT8/landmarks-regression-retail-0009.xml"
 # Useful incase of camera is set to flip horizontally.
 FLIP_INPUT_HORIZONTAL="False"
 # Input stream its value could be "CAM" or <video file link>
