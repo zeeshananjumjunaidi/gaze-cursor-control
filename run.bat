40db4c4905c7ed4abc95307eb11979954f7451bb
Zeeshan Anjum <zeeshan.anjum@eureka.me>
2020-07-04 10:37:16 +0300

    added windows support

---
+++ run.bat
@@ -0,0 +1,19 @@
+cls
+@ECHO OFF
+echo "Initializing Gaze Pointer Controller"
+REM Sourcing openvino
+REM "C:\Program Files (x86)\IntelSWTools\openvino\bin\setupvars.bat"
+
+set FACE_DETECTION_MODEL="models\intel\face-detection-adas-binary-0001\FP32-INT1\face-detection-adas-binary-0001.xml"
+set GAZE_ESTIMATION_MODEL="models\intel\gaze-estimation-adas-0002\FP32\gaze-estimation-adas-0002.xml"
+set HEAD_POSE_ESTIMATION_MODEL="models\intel\head-pose-estimation-adas-0001\FP32\head-pose-estimation-adas-0001.xml"
+set LANDMARKS_REGRESSION_MODEL="models\intel\landmarks-regression-retail-0009\FP32\landmarks-regression-retail-0009.xml"
+
+set FLIP_INPUT_HORIZONTAL="True"
+set INPUT="cam"
+REM set INPUT="bin\demo-win.mp4"
+REM Device to use for for inference, values are "CPU", "GPU", "HETERO:FPGA,CPU"
+set DEVICE="CPU"
+python src\main.py -d %DEVICE%  -fliph %FLIP_INPUT_HORIZONTAL% --previewFlags "fd" "hp" "fld" "ge" -i %INPUT% -f %FACE_DETECTION_MODEL% -g %GAZE_ESTIMATION_MODEL% -hp %HEAD_POSE_ESTIMATION_MODEL% -fl %LANDMARKS_REGRESSION_MODEL%
+
+echo "Program Finished"
