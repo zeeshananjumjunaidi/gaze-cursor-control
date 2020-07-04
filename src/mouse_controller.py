fb068b44108a13d46e1ac7aa49ba2fdc2628a3ca
Zeeshan Anjum <zeeshan.anjum@eureka.me>
2020-07-04 10:41:02 +0300

    add mouse control for windows

--- src/mouse_controller.py
+++ src/mouse_controller.py
@@ -8,13 +8,14 @@ will move the pointer.
 This class is provided to help get you started; you can choose whether you want to use it or create your own from scratch.
 '''
 import pyautogui
+import os
 
 class MouseController:
     def __init__(self, precision, speed):
         precision_dict={'high':100, 'low':1000, 'medium':500}
         speed_dict={'fast':1, 'slow':10, 'medium':5}
         self.max_width,self.max_height = pyautogui.size()
-        
+        self.is_windows = os.name == 'nt'
         #pyautogui.FAILSAFE=False
         self.precision=precision_dict[precision]
         self.speed=speed_dict[speed]
@@ -23,8 +24,10 @@ class MouseController:
     def move(self, x, y):
         dx=x
         dy=y
-        
-        current_x,current_y = pyautogui.mouseinfo.position()
+        if self.is_windows:
+            current_x,current_y = pyautogui.position()
+        else:
+            current_x,current_y = pyautogui.mouseinfo.position()
         if current_x-dx < 0:
             dx=0
         elif current_x+dx>=self.max_width:
